7ec6445c856696233d5ee155516b1a110cc01657
Hemantkumar Gaikwad <hemantkumargaikwad@gmail.com>
2022-07-29 23:39:54 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const axios = require('axios'); //the import statement for axios
 const path = require('path');
+
 // Section 2 we are calling the express function to initialize the application
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
 // Section 3 we are sending back HTML content with the h1 tag by adding it to the response object when we access the application
 app.get('/', (req, res) => { 
  res.send("<h1>Home page</h1>");
@@ -15,7 +17,9 @@ app.get('/users', (req, res) => {
        res.send(response.data);
      });
    });
+
 // Section 4 we are starting the express js server on port 3000
-app.listen(3000, () => {
- console.log('server started on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+ console.log(`server started on port ${port}`);
 });
